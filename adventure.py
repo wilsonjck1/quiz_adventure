a0c980c55689c3503c2e9e4e92801071c7fc1261
jack wilson <jwilson19@VenerableBede.schools.sunderland.gov.uk>
2016-11-25 10:58:58 +0000

    The Project

--- adventure.py
+++ adventure.py
@@ -1,17 +1,206 @@
 # An adventure game
+from random import randint
 
+health = 100
+gold = 0
+if input == "balance".lower():
+   print(gold)
+
+def space():
+   print(" ")
+   print("Health: ", health, "Gold: ", gold)
+   print(" ")
+
+
+def startingroom():
+
+    global health
+    global gold
+    print("In the distance you see a treasure chest that could contain some loot and you decide to open it")
+    chest = randint(1, 2)
+
+    if chest == 1:
+        print("You have been rewarded with 10 gold, this could be useful later in your quest")
+        gold = gold + 10
+        room1()
+    else:
+        print("You found nothing")
+        room1()
+
+   
 def room1():
+    space()
+    print("You have left the starting room and are faced with 2 doors")
+    print("You can only choose one door")
+    print("door A leads to a dungeon")
+    print("door B leads to a tomb")
+    print("Which door do you enter")
 
-    ... make a game
+    door = input("Door: ")
+
+    if door.lower() == "a":
+       print("entering...")
+       dungeon()
+
+    elif door.lower() == "b":
+         print("entering...")
+         tomb()
+
+def dungeon():
+   space()
+   print("This route is blocked off, im afraid you have to head back and enter the crypt")
+   tomb()
+
+    
+def tomb():
+   space()
+   global health
+   restless_skeleton_hp = 30
+   restless_skeleton_damage = randint(10,20)
+   damage = randint(10,30)
+   hit_chance = randint(1,3)
+   print("You have entered the tomb!!!")
+   print("You hear a voice in the distance...")
+   print("Restless skeleton: HOW DARE YOU ENTER MY TOMB!!!")
+   print("Self conscience: oh no i need to find a weapon!")
+   print("Ahead of you there is two weapons")
+   print("A) a rusted sword")
+   print("B) or a hunting bow")
+   print("You can only choose one weapon to fight the Restless skeleton, choose wisely")
+
+   weapon = input("Weapon: ")
+   
+   if weapon.lower() == "a":
+      print("You have equiped the rusted sword")
+      print("You walk towards the skeleton")
 
 
 
+      print("The restless skeleton sudenly attacks")
+      if restless_skeleton_damage == 0:
+             print("You evaded his attack")
+      else:
+              print("You got hit")
+              health = health - restless_skeleton_damage
+
+
+      if 10<= damage <=15:
+              print("You slice your sword into the skeletons chest, but dont kill it")
+              print("it goes for another attack")
+              print("your health is", health)
+              restless_skeleton_hp = restless_skeleton_hp - damage
+
+            
+      if 15< damage <=20:
+              print("You stab your sword into the skeletons chest, but dont kill it")
+              print("it goes for another attack")
+              print("your health is", health)
+              restless_skeleton_hp = restless_skeleton_hp - damage
+
+      else:
+            print("You have slaughtered the skeleton and head towards the exit")
+            print("your health is", health)
+            print("exiting")
+            merchant()
+            
+
+      if health <= 0:
+             print("You died, try again")
+             
+
+
+   if weapon.lower() == "b":
+      print("You have equiped the hunting bow")
+      print("You walk  towards the skeleton")
 
 
 
+      print("The restless skeleton sudenly attacks")
+      if restless_skeleton_damage == 0:
+             print("You evaded his attack")
+      else:
+              print("You got hit")
+              health = health - restless_skeleton_damage
+
+
+      if 10<= damage <=15:
+              print("Your arrow scathes the skeletons arm, but dont kill it")
+              print("it goes for another attack")
+              print("your health is", health)
+              restless_skeleton_hp = restless_skeleton_hp - damage
+
+            
+      if 15< damage <=20:
+              print("Your arrow slices through the skeletons head, but somehow youdont kill it")
+              print("it goes for another attack")
+              print("your health is", health)
+              restless_skeleton_hp = restless_skeleton_hp - damage
+
+      else:
+            print("You have slaughtered the skeleton and head towards the exit")
+            print("your health is", health)
+            print("exiting")
+            merchant()
+
+      if health <= 0:
+             print("You died, try again")
+
+def merchant():
+   global health
+   global gold
+   space()
+   print("〠hola my friend〠")
+   print("Welcome to geralds gracious goods")
+   print("Would you like to purchase a health potion my jolly good friend")
+   print("A) Yes")
+   print("B) No")
+
+   answer = input("Answer: ")
+
+   if answer.lower() == "a":
+    print("Do you have the gold my friend?")
+    print("...")
+
+    if gold == 10:
+      print("Ah yes you do, here you go, enjoy the potion!")
+      print("... 10 Gold coins removed ...")
+      bossroom()
+
+    if gold == 0:
+       print("Im afraid you havent got the gold my friend, potter along and earn money you peasant")
+
+    if  answer.lower() == ("b"):
+       print("You're really missing out my friend!")
+       print("Goodbye! Have great time and enjoy your inevitable death which could have been prevented had you bought one of gerald's gracious goods potions")
+       bossroom()
+
+def bossroom():
+    global health
+    global gold
+    gold = gold - 10
+    space()
+    
+     
+    print("your health is only", health, "would you like to drink your potion now or save it until later?")
+    print("A) Yes")
+    print("B) No")
+
+    answer = input("Answer: ")
+
+    if answer.lower() == "a":
+     health = health + 50
+    print("Health regenerating... your health is now", health,)
+
+    if answer.lower() == "b":
+    print("Storing Health potion...")
+    
+
+   
+   
 
 
 # Leave this at the bottom - it makes room1 run automatically when you
 # run your code.
 if __name__ == "__main__":
-    room1()
+    startingroom()
+    
--- quiz.py
+++ quiz.py
@@ -1,11 +1,94 @@
 # Our quiz!
+score=0
 
 def quiz():
+    global score
+    global name
+    name = input("Enter your name here: ")
 
-    ... make a quiz!
+
+    question1()
+    question2()
+    question3()
+
+    print(name, "scored", score, "points, try again")
+    if score == 3:
+        print("Winner")
+
+    else:
+        print("Loser")
+
+    
+
+    
+
+    
+def question1():
+    global score
+    global name
+    print("Here is your first question,", name)
+    print(" ")
+    print("==========================================================================")
+    print(" ")
+    print("1)What is The most popular cheese?")
+    print("A. Blue Cheese")
+    print("B. Goats Cheese")
+    print("C. Cottage Cheese")
+    print("D. Harry Tiltmans homemade cheese")
+
+    answer = input("Answer:")
+
+    if answer.upper() == "D":
+        score == score+1
+        print("Well Done, Have a singular point!")
+
+    else:
+        print("Your wrong Mate")
+def question2():
+    global score
+    global name
+    print(" ")
+    print("==========================================================================")
+    print(" ")
+    print("2) What is Donald Trumps youngest son named?")
+    print("A. Sassy")
+    print("B. Barron")
+    print("C. Legolas")
+    print("D. Lezley")
+    print("E. Donald Trump Junior Junior")
+
+    answer = input("Answer: ")
+
+    if answer.upper() == "B":
+        score=score+2
+        print("Well Done mate, have another point")
+
+    else:
+        print("You've only gone at got it wrong!")
+
+def question3():
+    global score
+    global name
+    print(" ")
+    print("==========================================================================")
+    print(" ")
+    print("3) which car is the most expensive")
+    print("A. BMW i8")
+    print("B. Corvette z06")
+    print("C. nissan skyline gtr 2016")
+    print("D. Ford Mustang 2016")
+
+    answer = input("Answer: ")
+
+    if answer.upper() == "A":
+        score=score + 1
+        print("Nice one mate, have another point!")
+
+    else:
+        print("Mission failed, try harder next time:( ")
 
 
-
+    
 
 
 # Leave this at the bottom - it makes quiz run automatically when you
